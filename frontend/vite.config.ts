76a45afffdf4fa3a7a6ac07ef8abeee95cc9784b
Desstrozon <147873488+Desstrozon@users.noreply.github.com>
2025-11-28 21:21:08 +0100

    Refactor Vite config to use mode-based base path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,26 +2,28 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+ 
+  base: mode === "production" ? "/frontend/" : "/",
+
   server: {
     host: "0.0.0.0",
     port: 8080,
     proxy: {
       "/api": {
-        target: "http://192.168.1.50:8000", // BACKEND en tu LAN
+        target: "http://192.168.1.50:8000",
         changeOrigin: true,
       },
       "/storage": {
-        target: "http://192.168.1.50:8000", // para imágenes de Storage
+        target: "http://192.168.1.50:8000",
         changeOrigin: true,
       },
     },
   },
-
-});
+}));
