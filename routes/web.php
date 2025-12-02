d6b9d7fd4c98e5561e54761fe4a07fcdec11a405
Desstrozon <147873488+Desstrozon@users.noreply.github.com>
2025-12-02 22:30:47 +0100

    Fix: Usar response()->file() para servir el index.html correctamente

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return file_get_contents(public_path('frontend/index.html'));
+    return response()->file(public_path('frontend/index.html'));
 })->name('inicio');
 
 Route::middleware([
